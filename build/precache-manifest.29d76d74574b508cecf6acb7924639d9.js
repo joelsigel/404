cbee1450f74078a302475d89b5202642970b75ad
“Joel <joelsigel@gmail.com>
2019-03-04 13:01:31 -0700

    react framework build/test 0.5011

---
+++ build/precache-manifest.29d76d74574b508cecf6acb7924639d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "38e80ebee3db4bbc0b83",
+    "url": "/static/js/main.38e80ebe.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "38e80ebee3db4bbc0b83",
+    "url": "/static/css/main.0820376d.chunk.css"
+  },
+  {
+    "revision": "b259e424f9ec1056c9a1570d17aefa37",
+    "url": "/index.html"
+  }
+];
